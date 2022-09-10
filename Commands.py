a047ac9296e191d7df2a3c068117867fca3a019b
Raconeur <cameronemrie1@gmail.com>
2022-09-10 18:19:34 -0500

    moved things

--- Commands.py
+++ Commands.py
@@ -17,105 +17,107 @@ from discord.ext.commands import has_permissions, MissingPermissions
 
 class Commands:
     
-    @client.command(name='purge', aliases=['clear'])
-    @commands.has_permissions(manage_messages=True)
-    async def purge_chat_command(msg: discord.Message, arg):
-        amount = await msg.channel.purge(limit=int(arg) + 1)
-        date = datetime.datetime.now()
-        amountDeleted = format(len(amount) - 1)
-        sentMessage = await msg.channel.send("Purged " + str(amountDeleted) + " messages @ " + str(date.year) + "-" + str(date.month) + "-" + str(date.day) + " " + str(date.hour) + ":" + str(date.minute))
-        messageId = sentMessage.id
-        messageToDelete = await msg.channel.fetch_message(messageId)
-        time.sleep(5)
-        await messageToDelete.delete()
+    async def runCommand(name:str,user:discord.user):
+        
+        if (name == "purge"):
+            @commands.has_permissions(manage_messages=True)
+            async def purge_chat_command(msg: discord.Message, arg):
+                amount = await msg.channel.purge(limit=int(arg) + 1)
+                date = datetime.datetime.now()
+                amountDeleted = format(len(amount) - 1)
+                sentMessage = await msg.channel.send("Purged " + str(amountDeleted) + " messages @ " + str(date.year) + "-" + str(date.month) + "-" + str(date.day) + " " + str(date.hour) + ":" + str(date.minute))
+                messageId = sentMessage.id
+                messageToDelete = await msg.channel.fetch_message(messageId)
+                time.sleep(5)
+                await messageToDelete.delete()
 
-    
-    
-    @client.command(pass_context=True)
-    @commands.has_permissions(manage_messages=True) # Change to manage members
-    async def addVip(ctx, *, user: discord.User):
-        member = user
-        role = discord.utils.get(member.guild.roles, name="VIP")
-        await member.add_roles(role)
-        
-    
-    @client.command()
-    @has_permissions(manage_messages=True) # Change to manage members
-    async def commandsList(ctx):
-        embed=discord.Embed(title="Warden Commands", description="*admins only*", color=0xff0000)
-        embed.add_field(name="*>addgems(@, amount)*", value="Adds an amount of @'s gems", inline=True)
-        embed.add_field(name=" *>removegems(@, amount)*", value="Removes an amount of @'s gems", inline=True)
-        embed.add_field(name="*>setgems(@, amount)*", value="Sets the @'s gem amount", inline=True)
-        embed.add_field(name=">gems", value="Says the amount of gems the user has ", inline=True)
-        embed.add_field(name="*>clear (amount)*", value="Clears the amount of msgs in chat", inline=True)
-        embed.add_field(name="*>addVip (@)*", value="Gives VIP role to the @", inline=True)
-        embed.add_field(name="*>roulette*", value="Initate roulette immedietdly", inline=True)
-        embed.add_field(name="*>chatchecker*", value="Initiate chat algorithm immedietly ", inline=True)
-        embed.add_field(name="*>debug*", value="Senbds debug msg previously editted", inline=True)
-        embed.add_field(name=">shop", value="Shows the items in the shop", inline=True)
-        embed.add_field(name=">buy (item name)", value="Purchases the item you input", inline=True)
-        await ctx.send(embed=embed)
-        
-    @client.command()
-    @has_permissions(manage_messages=True)
-    async def shop(ctx):
-        # String will be pulled from db and determine the items that rotate 
-        await ctx.send("""
-                            ```   𝐈𝐭𝐞𝐦:              𝐂𝐨𝐬𝐭: 
-    """ + "- Kitpvp Cash" + "      "  + "20 Gems per $" 
-        +  '\n' + " - VIP Role" + "      "  + "50,000 Gems"
-        +  '\n' + " - Warden Tag" + "      "  + "25,000 Gems"  +  """```""")
-        
-        
-    @client.command()
-    @has_permissions(manage_messages=True)
-    async def buy(ctx, *, state):
-        wardenChannel = client.get_channel(849510501209473034)
-        id = ctx.author.id
-        c.execute('SELECT * FROM warden WHERE id IN (SELECT id FROM warden WHERE id = ?)', (id,))
-        testId = c.fetchall()
-        playerAmount = getInstanceInTuple(testId, 2)
-        playerAmount =  float(playerAmount)
-        args = state.split(" ")
-        primaryCmd = args[0]
-        secondaryCmd = args[1]
-        if (primaryCmd.lower() == "kitpvp"):
-            tertiaryCmd = args[2]
-            cost = int(tertiaryCmd)
-            if (playerAmount >= cost):
-                gemsEdit(id,cost,False)
-                await wardenChannel.send("""
-                ```Discord name: """ + str(ctx.author) + 
-                '\n' + """Kitpvp Cash Owed: """ + str(cost / 20) + """```""")
-                await ctx.send(str(cost / 20) + " Kitpvp Cash purchased!")
-            else:
-                await ctx.send("Insufficient gems, you have " + str(playerAmount) + ".")
-        if (primaryCmd.lower() == "vip"):
-            cost = 500000
-            if (playerAmount >= cost):
-                gemsEdit(id,cost,False)
-                member = ctx.message.author
-                role = discord.utils.get(member.guild.roles, name = "VIP")
-                await member.add_roles(role)
-                await ctx.send("VIP role purchased!")
-            else:
-                await ctx.send("Insufficient gems, you have " + str(playerAmount) + ".")
-        if (primaryCmd.lower() == "warden"):
-            cost = 25000
-            if (playerAmount >= cost):
-                gemsEdit(id,cost,False)
-                await wardenChannel.send("""
-                ```Discord name: """ + str(ctx.author) + 
-                '\n' + """Owed: """ + """Warden Tag""" + """```""")
-                await ctx.send("Warden tag purchased!")
-            else:
-                await ctx.send("Insufficient gems, you have " + str(playerAmount) + ".")        
-                
-                
-    @client.command()
-    @has_permissions(manage_messages=True)
-    async def debug(ctx):
-        embed=discord.Embed(title="Warden", description="Testing description", color=0xe10e63)
-        embed.set_thumbnail(url="https://images-ext-2.discordapp.net/external/O3UgL5cSIxuehkd98p3Esd-Vh7vQwnA4BVXtanKdzYc/%3Fsize%3D256/https/cdn.discordapp.com/avatars/679812786200510583/07fbc40bccdef5e7873f745ec719d69d.png")
-        embed.add_field(name="Field 1", value="2", inline=False)
-        await ctx.send(embed=embed)
+
+
+        @client.command(pass_context=True)
+        @commands.has_permissions(manage_messages=True) # Change to manage members
+        async def addVip(ctx, *, user: discord.User):
+            member = user
+            role = discord.utils.get(member.guild.roles, name="VIP")
+            await member.add_roles(role)
+            
+
+        @client.command()
+        @has_permissions(manage_messages=True) # Change to manage members
+        async def commandsList(ctx):
+            embed=discord.Embed(title="Warden Commands", description="*admins only*", color=0xff0000)
+            embed.add_field(name="*>addgems(@, amount)*", value="Adds an amount of @'s gems", inline=True)
+            embed.add_field(name=" *>removegems(@, amount)*", value="Removes an amount of @'s gems", inline=True)
+            embed.add_field(name="*>setgems(@, amount)*", value="Sets the @'s gem amount", inline=True)
+            embed.add_field(name=">gems", value="Says the amount of gems the user has ", inline=True)
+            embed.add_field(name="*>clear (amount)*", value="Clears the amount of msgs in chat", inline=True)
+            embed.add_field(name="*>addVip (@)*", value="Gives VIP role to the @", inline=True)
+            embed.add_field(name="*>roulette*", value="Initate roulette immedietdly", inline=True)
+            embed.add_field(name="*>chatchecker*", value="Initiate chat algorithm immedietly ", inline=True)
+            embed.add_field(name="*>debug*", value="Senbds debug msg previously editted", inline=True)
+            embed.add_field(name=">shop", value="Shows the items in the shop", inline=True)
+            embed.add_field(name=">buy (item name)", value="Purchases the item you input", inline=True)
+            await ctx.send(embed=embed)
+            
+        @client.command()
+        @has_permissions(manage_messages=True)
+        async def shop(ctx):
+            # String will be pulled from db and determine the items that rotate 
+            await ctx.send("""
+                                ```   𝐈𝐭𝐞𝐦:              𝐂𝐨𝐬𝐭: 
+        """ + "- Kitpvp Cash" + "      "  + "20 Gems per $" 
+            +  '\n' + " - VIP Role" + "      "  + "50,000 Gems"
+            +  '\n' + " - Warden Tag" + "      "  + "25,000 Gems"  +  """```""")
+            
+            
+        @client.command()
+        @has_permissions(manage_messages=True)
+        async def buy(ctx, *, state):
+            wardenChannel = client.get_channel(849510501209473034)
+            id = ctx.author.id
+            c.execute('SELECT * FROM warden WHERE id IN (SELECT id FROM warden WHERE id = ?)', (id,))
+            testId = c.fetchall()
+            playerAmount = getInstanceInTuple(testId, 2)
+            playerAmount =  float(playerAmount)
+            args = state.split(" ")
+            primaryCmd = args[0]
+            secondaryCmd = args[1]
+            if (primaryCmd.lower() == "kitpvp"):
+                tertiaryCmd = args[2]
+                cost = int(tertiaryCmd)
+                if (playerAmount >= cost):
+                    gemsEdit(id,cost,False)
+                    await wardenChannel.send("""
+                    ```Discord name: """ + str(ctx.author) + 
+                    '\n' + """Kitpvp Cash Owed: """ + str(cost / 20) + """```""")
+                    await ctx.send(str(cost / 20) + " Kitpvp Cash purchased!")
+                else:
+                    await ctx.send("Insufficient gems, you have " + str(playerAmount) + ".")
+            if (primaryCmd.lower() == "vip"):
+                cost = 500000
+                if (playerAmount >= cost):
+                    gemsEdit(id,cost,False)
+                    member = ctx.message.author
+                    role = discord.utils.get(member.guild.roles, name = "VIP")
+                    await member.add_roles(role)
+                    await ctx.send("VIP role purchased!")
+                else:
+                    await ctx.send("Insufficient gems, you have " + str(playerAmount) + ".")
+            if (primaryCmd.lower() == "warden"):
+                cost = 25000
+                if (playerAmount >= cost):
+                    gemsEdit(id,cost,False)
+                    await wardenChannel.send("""
+                    ```Discord name: """ + str(ctx.author) + 
+                    '\n' + """Owed: """ + """Warden Tag""" + """```""")
+                    await ctx.send("Warden tag purchased!")
+                else:
+                    await ctx.send("Insufficient gems, you have " + str(playerAmount) + ".")        
+                    
+                    
+        @client.command()
+        @has_permissions(manage_messages=True)
+        async def debug(ctx):
+            embed=discord.Embed(title="Warden", description="Testing description", color=0xe10e63)
+            embed.set_thumbnail(url="https://images-ext-2.discordapp.net/external/O3UgL5cSIxuehkd98p3Esd-Vh7vQwnA4BVXtanKdzYc/%3Fsize%3D256/https/cdn.discordapp.com/avatars/679812786200510583/07fbc40bccdef5e7873f745ec719d69d.png")
+            embed.add_field(name="Field 1", value="2", inline=False)
+            await ctx.send(embed=embed)
--- Warden.py
+++ Warden.py
@@ -21,13 +21,15 @@ from discord.ext.commands import has_permissions, MissingPermissions
 from discord.ext.commands import bot
 from discord.webhook import AsyncWebhookAdapter
 
+import Commands as commands
+
 if os.path.exists(os.getcwd() + "/config.json"):
     
     with open("./config.json") as f:
         configData = json.load(f)
 
 else:
-    configTemplate = {"Token": "", "Prefix": "!"}
+    configTemplate = {"Token": "", "Prefix": ">"}
 
     with open(os.getcwd() + "/config.json", "w+") as f:
         json.dump(configTemplate, f) 
@@ -52,6 +54,8 @@ c = conn.cursor()
 #        gems integer)""")
 
 #c.execute(""" CREATE TABLE IF NOT EXISTS wardenMoniter ( edtrhg
+
+commands.
 
 @client.event
 async def on_ready():
@@ -89,115 +93,6 @@ async def ping_server_command(msg):
 
 # CTRL + K then CTRL + C to mass comment
 
-
-# @client.command()
-# @has_permissions(manage_messages = True) 
-# async def chatchecker(ctx):
-#     print("Starting")
-#     global chatChannels
-#     for z in chatChannels:
-#         global msgStringCache
-#         channel = client.get_channel(z) #This is the general channel, when we check other channels I can create a sorting algorithm for multiple ones and substitute
-#         c.execute('SELECT * FROM wardenMoniter WHERE general = general')
-#         startMsg = c.fetchall()
-#         startMsg = startMsg[0]
-#         startMsg = str(startMsg)
-#         startMsg = cleanDbTable(startMsg)
-#         startMsg = datetime.datetime.strptime(startMsg, '%Y-%m-%d %H:%M:%S.%f')
-#         messages = await channel.history(after=startMsg).flatten()
-#         finalMsg = (messages[len(messages) - 1])
-#         finalMsgTime = finalMsg.created_at
-#         for a in messages:
-#             if (a.author.bot != True):
-#                 senderId = a.author.id
-#                 senderName = str(a.author)
-#                 senderName = senderName.replace(" ","")
-#                 senderMsg = a.content
-#                 senderTime = a.created_at
-#                 senderSeconds = float(senderTime.timestamp())
-#                 senderTime = float(senderSeconds)
-#                 strMsgData = senderName + ":" + str(senderTime) + ":" + str(senderId) + ":" + "1"
-#                 for b in msgStringCache:
-#                     if (b != strMsgData or msgStringCache == []):
-#                         strMsgData = senderName + ":" + str(senderTime) + ":" + str(senderId) + ":" + "1"
-#                         msgStringCache.append(strMsgData)
-#                 x = 0
-#                 print(msgStringCache)
-#                 while x < len(msgStringCache):
-#                     scan = msgStringCache[x].split(":")
-#                     scanName = scan[0]
-#                     if (str(scanName) == str(senderName)):
-#                         senderData = msgStringCache[x]
-#                         x = len(msgStringCache)
-#                     else:
-#                         x = x + 1
-#                 args = senderData.split(":")
-#                 senderTimeDb = float(args[1])
-#                 senderIdDb = int(args[2])
-#                 senderPointsDb = int(args[3])
-#                 if (len(senderMsg) > 1):
-#                     if (senderTime - senderTimeDb > 2.5 or senderTimeDb == 0):
-#                         senderPointsDb = senderPointsDb + 1
-#                     senderTimeDb = senderTime
-#                 x = 0
-#                 while x < len(msgStringCache):
-#                     scan = msgStringCache[x].split(":")
-#                     scanName = scan[0]
-#                     if (str(scanName) == str(senderName)):
-#                         strMsgData = str(senderName) + ":" + str(senderTimeDb) + ":" + str(senderId) + ":" + str(senderPointsDb)
-#                         msgStringCache[x] = strMsgData
-#                         x = len(msgStringCache)
-#                     else:
-#                         x = x + 1
-#                 for x in msgStringCache:
-#                     args = x.split(":")
-#                     vicName = args[0]
-#                     vicId = args[2]
-#                     validMsg = args[3]
-#                     vicId = int(vicId)
-#                     validMsg = int(validMsg)
-#                     rewardGems(vicId, validMsg, vicName)
-#     msgStringCache.clear()
-#     clearMsgDb()
-#     generalLastMsg(finalMsgTime)    
-#     print("Finished Algorithm")
-
-# def clearMsgDb():
-#     with conn:
-#         c.execute('DELETE FROM wardenMessages')
-#     print("Cleared wardenMessages db")
-
-# def rewardGems(id, msgs, name):
-#         gemsAdd = 0
-#         b = 0
-#         msgCount = msgs
-#         while b < 1:
-#             if (msgCount >= 3):
-#                 gemsAdd += .2
-#                 msgCount = msgCount - 3
-#             if (msgCount == 0):
-#                 b = 1
-#             if (msgCount <= 2):
-#                 gemsAdd += .1
-#                 msgCount = msgCount - 2
-#                 b = 1
-#         round(gemsAdd)
-#         c.execute('SELECT * FROM warden WHERE id IN (SELECT id FROM warden WHERE id = ?)', (id,))
-#         testId = c.fetchall()
-#         if (testId == []):
-#             c.execute("INSERT INTO warden VALUES (:id, :name, :gems)", {'id': id, 'name': name, 'gems': 0})
-#             print("Addition Successful")       
-#         with conn:
-#             c.execute(
-#             'UPDATE warden SET gems = gems + ? WHERE id=?', 
-#             (round(gemsAdd), id,))
-
-# def generalLastMsg(time):
-#     with conn:
-#         c.execute(
-#         'UPDATE wardenMoniter SET general = ?',
-#         (time,)) 
-#         print("Updated general final time.") 
 
 @client.command()
 @has_permissions(manage_messages=True)
