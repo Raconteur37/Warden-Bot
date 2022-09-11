a360979ddbdfe2bec431900b0a2b75c6125d5a93
Cam <cameronemrie1@gmail.com>
2022-09-10 19:12:32 -0500

    trying to fix commands

--- Warden.py
+++ Warden.py
@@ -3,25 +3,10 @@ import os
 import json
 import discord
 # python3 -m pip install (package).py fixes many problems
-import time
-import datetime
-from discord import message
-from discord import user
-from discord import channel
-from discord.client import Client
-from discord.enums import SpeakingState  
 from discord.ext import commands
 import sqlite3
-import random
-from time import sleep
-from discord.ext import tasks, commands
-from discord.utils import get
-
-from discord.ext.commands import has_permissions, MissingPermissions
-from discord.ext.commands import bot
-from discord.webhook import AsyncWebhookAdapter
-
-import Commands as commands
+from discord.ext import commands
+from discord.ext.commands import has_permissions
 
 if os.path.exists(os.getcwd() + "/config.json"):
     
@@ -37,43 +22,35 @@ else:
 token = configData["Token"]
 prefix = configData["Prefix"]
 
-client = commands.Bot(prefix)
+bot = commands.Bot(command_prefix='>')
 
 conn = sqlite3.connect('warden.db')
 c = conn.cursor()
 
-#.execute(""" CREATE TABLE IF NOT EXISTS wardenMessages (
-#    id integer,
-#    name text,
-#    msg text,
-#    time integer)""")
 
-#c.execute(""" CREATE TABLE IF NOT EXISTS warden (
-#        id integer,
-#        name text,
-#        gems integer)""")
-
-#c.execute(""" CREATE TABLE IF NOT EXISTS wardenMoniter ( edtrhg
-
-commands.
-
-@client.event
+@bot.event
 async def on_ready():
     activity = discord.Activity(name=" channels...", type=3)
-    await client.change_presence(status=discord.Status.do_not_disturb, activity=activity)
+    await bot.change_presence(status=discord.Status.do_not_disturb, activity=activity)
     # await guessWordGame.start()
-    print('Bot is now online')
+    print(f'Warden is now online')
 
     #
     # Commands
     #
+    
+@bot.command(pass_context=True)
+async def ping(ctx):
+	await ctx.send("pong")
+ 
+
 
 # TODO make it so all command add you to the data base and still have propper function if said person isnt in it already
 
 # Adds one gem and adds player to db if they are not there
 
 
-@client.command(name='online')
+@bot.command(name='online')
 async def ping_server_command(msg):
 
     hostname = "mtl.renatusnetwork.com"
@@ -94,7 +71,7 @@ async def ping_server_command(msg):
 # CTRL + K then CTRL + C to mass comment
 
 
-@client.command()
+@bot.command()
 @has_permissions(manage_messages=True)
 async def shop(ctx):
     # String will be pulled from db and determine the items that rotate 
@@ -106,9 +83,9 @@ async def shop(ctx):
 
 
 
-@client.event
+@bot.event
 async def on_command_error(msg, error):
     await msg.channel.send(str(error))
 
 # chatchecker()
-client.run(token)
+bot.run(token)
